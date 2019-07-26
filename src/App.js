f5451298063e6dd4ad77b746cb932ab22eacd9e6
Jacob Carver <jacxbcarver@gmail.com>
2019-07-25 18:05:36 -0600

    added brightness controller

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 // API Config
 import url from './config';
 
 // Components
 import ColorPicker from './components/ColorPicker';
+import ToggleSwitch from './components/ToggleSwitch';
+import BrightnessController from './components/BrightnessController';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			API_URL: url,
-			lights: []
+			API_URL: url
 		}
 	}
-	componentWillMount() {
-		axios.get(this.state.API_URL)
-			.then((res) => { return res.data })
-			.then((data) => {
-				let lights = []
-				for (var i = 1; i <= Object.keys(data.lights).length; i++) {
-					lights.push(data.lights[`${i}`]);
-				}
-				this.setState({ lights });
-			})
-			.catch(() => {
-				console.log('ERROR')
-			})
-	}
 	render() {
-		const { API_URL, lights } = this.state;
+		const { API_URL } = this.state;
 		return (
 			<div className="App">
 				<ColorPicker API_URL={API_URL} />
-				<h1>Total Lights: {lights.length} </h1>
-				<div className="lights">
-					{lights.map((light, i) => {
-						console.log(light)
-						return (
-							<div key={i} className="light">
-								<h4>{light.productname}</h4>
-								<i className={`fas ${light.productname === 'Hue color lamp' ? 'fa-lightbulb' : light.productname === 'Hue go' ? 'fa-adjust' : ''}`}></i>
-							</div>
-						)
-					})}
-				</div>
+				<ToggleSwitch API_URL={API_URL} />
+				<BrightnessController API_URL={API_URL} />
 			</div>
 		);
 	}
---
+++ src/components/BrightnessController.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class BrightnessController extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			API_URL: this.props.API_URL,
+			brightness: 0,
+			bri: 0,
+			allLights: '/groups/1/action'
+		}
+		this.changeBrightness = this.changeBrightness.bind(this);
+	}
+	// Change Brightness With Slider
+	changeBrightness(e) {
+		var brightness = e.target.value;
+		var bri = Math.round((brightness * 255) / 100);
+		if (brightness !== this.state.brightness) {
+			this.setState({
+				brightness: brightness,
+				bri: bri
+			})
+			axios({
+					method: 'put',
+					url: this.state.API_URL + this.state.allLights,
+					data: {
+						bri
+					}
+				})
+				.catch(err => console.log(err));
+		}
+	}
+	componentWillMount() {
+		axios.get(this.state.API_URL)
+			.then((res) => { return res.data })
+			.then((data) => {
+				this.setState({ brightness: Math.round((data.groups[1].action.bri / 255) * 100) })
+			})
+	}
+	render() {
+		return (
+			<div id="brightness-controller">
+				<div className="container">
+                    <span>Brightness: {this.state.brightness}%</span>
+                    <input onChange={this.changeBrightness} type="range" min="0" max="100" value={this.state.brightness} className="slider"></input>
+                </div>
+			</div>
+		)
+	}
+}
+
+export default BrightnessController;
